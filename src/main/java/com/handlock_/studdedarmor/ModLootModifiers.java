41eaf842af0c2dda47262f197fa09233a9536a61
Hand-Lock <weeklykhan16@gmail.com>
2025-05-18 18:08:02 +0200

    added studded template to suspicious blocks

---
+++ src/main/java/com/handlock_/studdedarmor/ModLootModifiers.java
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ * Copyright (C) 2025 HandLock_
+ */
+package com.handlock_.studdedarmor;
+
+import com.mojang.serialization.Codec;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import com.handlock_.studdedarmor.loot.StuddedTemplateLootModifier;
+
+/** Registrazione di tutti i Global Loot Modifier del mod. */
+public final class ModLootModifiers {
+
+    /** Registry dei codec (Forge 47 usa questa chiave). */
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, StuddedArmor.MOD_ID);
+
+    /** Codec del loot-modifier che aggiunge lo smithing template. */
+    public static final RegistryObject<Codec<StuddedTemplateLootModifier>> STUDDED_TEMPLATE =
+            LOOT_MODIFIERS.register("studded_template", () -> StuddedTemplateLootModifier.CODEC);
+
+    private ModLootModifiers() { /* no-instantiation */ }
+
+    public static void register(IEventBus bus) {
+        LOOT_MODIFIERS.register(bus);
+    }
+}
